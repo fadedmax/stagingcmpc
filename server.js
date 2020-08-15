0607a57d014a3134474d1f47ceee735e9a380b5d
Glitch (hello-webpage) <none>
2020-08-15 19:54:20 +0000

    🕋🏆 Checkpoint ./package.json:757370/282:2785459/20 ./server.js:757370/222:2785459/112 ./static/index.html:757370/5488 ./static/script.js:757370/160

---
+++ server.js
@@ -0,0 +1,10 @@
+
+// hey dude, you do relaise i sent u the wrong link right
+// then just copy some of these over and it should work fine
+// let me give you the real link one second.
+// ok dad
+
+let express = require('express');
+let server = express();
+
+server.use(express.static('./static'));
